57e805b712247e68590bea67769541b648d29936
shemuel_uriya_ashkenazy <uriyaas@gmail.com>
2021-07-04 15:06:43 +0300

    routes.js

--- server.js
+++ server.js
@@ -37,8 +37,9 @@ app.use(passport.initialize());
 app.use(passport.session()); // persistent login sessions
 app.use(flash()); // use connect-flash for flash messages stored in session
 
+
 // routes ======================================================================
-//require('./app/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
+require('./app/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
 
 // launch ======================================================================
 app.listen(port);
